0dc42251bb1b14c524d05dcd84fed77492d19bf4
BananaBaileys <tian987654321@gmail.com>
2023-06-15 12:18:10 -0400

    add delete function
    title

--- src/App.js
+++ src/App.js
@@ -29,6 +29,13 @@ export default function App() {
     })
   }
 
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -49,7 +56,8 @@ export default function App() {
                 onChange={e => toggleTodo(todo.id, e.target.checked)}/>
                 {todo.title}
               </label>
-              <button className="btn btn-danger">Delete</button>
+              <button onClick={() => deleteTodo(todo.id)}
+              className="btn btn-danger">Delete</button>
             </li>
           )
         })}   
